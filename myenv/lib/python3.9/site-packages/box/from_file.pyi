677e8972cf8a7bd8d8e4895ed9e9c7ef6f956a7f
estevam5s <estevamsouzalaureth@gmail.com>
2023-05-03 17:24:16 -0300

    🔒📝 @estevam5s atualizando o projeto. 👥📝 #feature #config #analise-dados

--- myenv/lib/python3.9/site-packages/box/from_file.pyi
+++
@@ -1,9 +0,0 @@
-from box.box import Box
-from box.box_list import BoxList
-from os import PathLike
-from typing import Any, Union
-
-def box_from_file(
-    file: Union[str, PathLike], file_type: str = ..., encoding: str = ..., errors: str = ..., **kwargs: Any
-) -> Union[Box, BoxList]: ...
-def box_from_string(content: str, string_type: str = ...) -> Union[Box, BoxList]: ...
