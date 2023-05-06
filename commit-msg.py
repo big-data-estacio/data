184bf416ff3890b527000bc87e64751e2c90a651
estevam5s <estevamsouzalaureth@gmail.com>
2023-05-06 17:36:29 -0300

    🔒📝 @ciclano atualizando o projeto. 👥📝 #feature #config #analise-dados

--- commit-msg.py
+++ commit-msg.py
@@ -1,25 +1,41 @@
 #!/usr/bin/env python3
 import sys
 import random
+import subprocess
 
+def get_changed_files():
+    output = subprocess.check_output(["git", "diff", "--cached", "--name-only"])
+    changed_files = output.decode("utf-8").splitlines()
+    return changed_files
+
+def get_custom_phrase(file_extension):
+    phrases = {
+        ".py": "Python file modified",
+        ".js": "JavaScript file modified",
+        ".html": "HTML file modified",
+        ".css": "CSS file modified",
+    }
+
+    return phrases.get(file_extension, "File modified")
 
 def main():
     commit_msg_filepath = sys.argv[1]
     with open(commit_msg_filepath, "r") as file:
         commit_msg = file.read()
 
-    # Lista de emojis
-    emojis = ["😀", "😃", "😄", "😁", "😆", "😅", "😂", "🤣", "🥲", "😊", "😇"]
+    changed_files = get_changed_files()
+    file_extensions = {file.split('.')[-1] for file in changed_files if '.' in file}
+    
+    custom_phrases = [get_custom_phrase(f".{ext}") for ext in file_extensions]
 
-    # Escolhe um emoji aleatório da lista
-    chosen_emoji = random.choice(emojis)
+    # Escolha um membro da equipe
+    team_member = "@fulano"
 
-    # Adiciona o emoji à mensagem de commit
-    new_commit_msg = f"{chosen_emoji} {commit_msg}"
+    # Gere a mensagem de commit personalizada
+    new_commit_msg = f"🔒📝 {team_member} atualizando o projeto. 👥📝 #feature #config #analise-dados"
 
     with open(commit_msg_filepath, "w") as file:
         file.write(new_commit_msg)
-
 
 if __name__ == "__main__":
     main()
