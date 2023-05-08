e56131b075f5087ecb69763d67b43bfbedd78c07
estevam5s <estevamsouzalaureth@gmail.com>
2023-05-08 09:39:01 -0300

    🐍 python file modified 📄 markdown file modified
    📄 markdown file modified
    🐍 python file modified

--- app.py
+++ app.py
@@ -1,52 +1,11 @@
-############################################################################################
-#                                   Packages                                               #
-############################################################################################
-
-
-import streamlit as st
-import client.setup as setup
-# import base64
-import plotly.express as px
 # from client.src.pages.👻_Login import login_page
-import os
-import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'client', 'src', 'pages')))
 from login import login_page
-
-
-
-############################################################################################
-#                                   Check Requirements                                     #
-############################################################################################
-
-
-# Lista de funções importadas
-funcoes_importadas = [
-    'streamlit',
-    'client.setup',
-]
-
-# Verifica se cada função está presente no arquivo requirements.txt
-faltando = []
-with open('requirements.txt') as f:
-    for line in f:
-        for funcao in funcoes_importadas:
-            if funcao in line:
-                break
-        else:
-            faltando.append(funcao)
-
-# Imprime as funções que não estão presentes no arquivo
-if faltando:
-    print('As seguintes funções não estão presentes no arquivo requirements.txt:')
-    print('\n'.join(faltando))
-else:
-    print('Todas as funções importadas estão presentes no arquivo requirements.txt.')
-
-
-############################################################################################
-#                                   Main                                                   #
-############################################################################################
+import streamlit as st
+import client.setup as setup
+import plotly.express as px
+import os
+import sys
 
 
 if __name__ == '__main__':
