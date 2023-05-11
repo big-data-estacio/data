b14037155cf1e974921d05301e5737f53b41c2b7
estevam5s <estevamsouzalaureth@gmail.com>
2023-05-11 01:46:58 -0300

    🐍 python file modified
    🐍 python file modified

--- client/setup.py
+++ client/setup.py
@@ -895,59 +895,54 @@ def mainLogin():
             db_deta_lucroliquido = deta.Base("lucroliquido")
 
             class DadosRestaurante:
-              def __init__(self):
-                self.data = pd.DataFrame()
+                def __init__(self):
+                    self.data = pd.DataFrame()
 
-              def load_data_from_deta(self):
-                items = db_deta_lucroliquido.fetch().items
-                self.data = pd.DataFrame(items)
+                def load_data_from_deta(self):
+                    items = list(db_deta_lucroliquido.fetch())
+                    self.data = pd.DataFrame(items)
 
-              def show_table(self):
-                st.write(self.data)
+                def add_data_to_deta(self, data):
+                    db_deta_lucroliquido.put(data)
 
-              def add_data_to_deta(self, data_dict):
-                db_deta_lucroliquido.put(data_dict)
+                def show_table(self):
+                    st.write(self.data)
 
-              def is_deta_empty(self):
-                items = db_deta_lucroliquido.fetch().items
-                return len(items) == 0
+                def is_deta_empty(self):
+                    return len(list(db_deta_lucroliquido.fetch())) == 0
+
 
             class AnaliseLucroLiquido:
-              def __init__(self, dados: DadosRestaurante):
-                  self.dados = dados
+                def __init__(self, dados: DadosRestaurante):
+                    self.dados = dados
 
-              def calcular_lucro_liquido(self):
-                if "Custos fixos" in self.dados.data and "Custos variáveis" in self.dados.data and "Receita total" in self.dados.data:
-                  custos_fixos = self.dados.data["Custos fixos"].sum()
-                  custos_variaveis = self.dados.data["Custos variáveis"].sum()
-                  receita_total = self.dados.data["Receita total"].sum()
-
-                  lucro_liquido = receita_total - custos_fixos - custos_variaveis
-
-                  return lucro_liquido
-                else:
-                  st.error("Os dados do DataFrame não contêm as colunas necessárias para calcular o lucro líquido.")
-                  return None
+                def calcular_lucro_liquido(self):
+                    return self.dados.data["lucro_liquido"].sum()
 
 
             def analise_lucro_liquido(dados: DadosRestaurante):
-              st.subheader("Análise de Lucro Líquido")
+                st.subheader("Análise de Lucro Líquido")
 
-              # Exibir dados em uma tabela
-              dados.show_table()
+                # Exibir dados em uma tabela
+                dados.show_table()
 
-              # Verificar se o banco de dados está vazio
-              if dados.is_deta_empty():
-                # Se estiver vazio, adicionar os dados
-                dados.add_data_to_deta({"lucro_liquido": 5000, "data": "2023-05-11"})
-                st.info("Dados adicionados ao banco de dados Deta com sucesso!")
-              else:
-                # Se não estiver vazio, apenas informar o lucro líquido
-                st.info("Lucro líquido já existe no banco Deta.")
+                # Verificar se o banco de dados está vazio
+                if dados.is_deta_empty():
+                    # Se estiver vazio, adicionar os dados
+                    dados.add_data_to_deta({"lucro_liquido": 5000, "data": "2023-05-11"})
+                    st.info("Dados adicionados ao banco de dados Deta com sucesso!")
+                else:
+                    # Se não estiver vazio, calcular e informar o lucro líquido
+                    analise = AnaliseLucroLiquido(dados)
+                    lucro_liquido = analise.calcular_lucro_liquido()
+                    st.write(f"Lucro líquido: R$ {lucro_liquido:.2f}")
+                    st.info("Lucro líquido já existe no banco Deta.")
+
 
             dados = DadosRestaurante()
             dados.load_data_from_deta()
             analise_lucro_liquido(dados)
+
 
 
 
