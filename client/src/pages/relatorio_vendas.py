8913cc131cceeb84b44126cb1df2fd594ba6af47
estevam5s <estevamsouzalaureth@gmail.com>
2023-05-10 21:54:32 -0300

    🐍 python file modified
    🐍 python file modified

---
+++ client/src/pages/relatorio_vendas.py
@@ -0,0 +1,11 @@
+from deta import Deta
+
+
+DETA_KEY = "e0u31gqkqju_2Ps7fJD5a1kAKF2Rr4Y31ASSdvUUeX8Y"
+deta = Deta(DETA_KEY)
+db = deta.Base("relatorio_vendas")
+
+
+# TODO - funçaõ relatorio de vendas
+def relatorioVenda():
+    pass
