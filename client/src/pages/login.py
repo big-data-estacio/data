ea41b3e0c700678a1ca8c3cb4fe38a31ba29c8e0
estevam5s <estevamsouzalaureth@gmail.com>
2023-05-11 18:03:11 -0300

    🐍 python file modified
    🐍 python file modified

--- client/src/pages/login.py
+++ client/src/pages/login.py
@@ -11,13 +11,29 @@ DETA_KEY = "e0u31gqkqju_2Ps7fJD5a1kAKF2Rr4Y31ASSdvUUeX8Y"
 # Initialize Deta
 deta = Deta(DETA_KEY)
 
-# Get database
 db = deta.Base("data")
+db_blocked = deta.Base("userbloqueado")
+
 
 def authenticate_user(username, password):
   user = db.get(username)
-  if user and user['password'] == password:
-    return True
+  if user:
+    if user['password'] == password:
+      return True
+    else:
+        # Se a senha estiver incorreta, aumentar o contador de falhas de login
+        user['failed_logins'] = user.get('failed_logins', 0) + 1
+        
+        # Se o usuário falhou na autenticação 3 vezes, bloqueá-lo
+        if user['failed_logins'] >= 3:
+          db_blocked.put(user)  # Adicionando o usuário ao banco de dados de usuários bloqueados
+          db.delete(username)  # Excluindo o usuário do banco de dados de usuários
+          # send_email(user)  # Enviar um email para o desenvolvedor
+          st.error("Usuário bloqueado após 3 tentativas falhas de login.")
+          return False
+        
+        db.put(user)  # Atualizando o contador de falhas de login no banco de dados de usuários
+        return False
   else:
     return False
 
@@ -42,4 +58,43 @@ def login_page():
         st.error("Por favor, insira um nome de usuário e senha.")
       elif username != "" and password != "":
         st.error("Nome de usuário ou senha incorretos.")
-  return False
+        st.info("Se você esqueceu sua senha, entre em contato com o administrador.")
+        if st.button("Deseja enviar um email para o administrador?"):
+          st.markdown("""
+          <style>
+            ul[class="css-j7qwjs e1fqkh3o7"]{
+              position: relative;
+              padding-top: 2rem;
+              display: flex;
+              justify-content: center;
+              flex-direction: column;
+              align-items: center;
+            }
+            .css-17lntkn {
+              font-weight: bold;
+              font-size: 18px;
+              color: grey;
+            }
+            .css-pkbazv {
+              font-weight: bold;
+              font-size: 18px;
+            }
+          </style>""", unsafe_allow_html=True)
+
+          st.header("Contact")
+
+          contact_form = """
+            <form action="https://formsubmit.co/{}" method="POST">
+              <input type="hidden" name="_captcha" value="false">
+              <input type="text" name="name" placeholder="Your name" required>
+              <br>
+              <input type="email" name="email" placeholder="Your email" required>
+              <br>
+              <textarea name="message" placeholder="Your message here"></textarea>
+              <br>
+              <button type="submit">Send</button>
+            </form>
+            """.format("estevamsouzalaureth@gmail.com")  # Substitua o endereço de e-mail aqui
+          st.markdown(contact_form, unsafe_allow_html=True)
+
+    return False
