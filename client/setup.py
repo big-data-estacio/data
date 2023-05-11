0878f604088e7f56bdaf61c815bacb42b6747abc
estevam5s <estevamsouzalaureth@gmail.com>
2023-05-11 02:27:36 -0300

    🐍 python file modified
    🐍 python file modified

--- client/setup.py
+++ client/setup.py
@@ -822,27 +822,27 @@ def mainLogin():
 
 
           if selecionar == "Análise de lucro líquido":
-            #  lucro_liquido.analise_lucro_liquido()
-            # db_deta_lucroliquido = deta.Base("lucroliquido")
-            # def calculate_net_profit():
-            #   # Buscando todos os dados do banco Deta
-            #   items = db_deta_lucroliquido.fetch().items
+            lucro_liquido.analise_lucro_liquido()
+            db_deta_lucroliquido = deta.Base("lucroliquido")
+            def calculate_net_profit():
+              # Buscando todos os dados do banco Deta
+              items = db_deta_lucroliquido.fetch().items
               
-            #   # Se houver itens, calcular o lucro líquido e exibi-lo
-            #   if items:
-            #     # Criando um DataFrame com os dados
-            #     data = pd.DataFrame([item for item in items])
+              # Se houver itens, calcular o lucro líquido e exibi-lo
+              if items:
+                # Criando um DataFrame com os dados
+                data = pd.DataFrame([item for item in items])
                 
-            #     # Calculando o lucro líquido
-            #     lucro_liquido = data['lucro_liquido'].sum()
+                # Calculando o lucro líquido
+                lucro_liquido = data['lucro_liquido'].sum()
                 
-            #     # Criando um DataFrame para exibir o lucro líquido
-            #     df_lucro_liquido = pd.DataFrame({'Lucro Líquido': [lucro_liquido]})
+                # Criando um DataFrame para exibir o lucro líquido
+                df_lucro_liquido = pd.DataFrame({'Lucro Líquido': [lucro_liquido]})
                 
-            #     # Exibindo o DataFrame
-            #     st.dataframe(df_lucro_liquido)
+                # Exibindo o DataFrame
+                st.dataframe(df_lucro_liquido)
 
-            # calculate_net_profit()
+            calculate_net_profit()
 
             pass
 
