94a62b3b6ba74ccfb0f8f91e0829c66e66b2a385
estevam5s <estevamsouzalaureth@gmail.com>
2023-05-13 01:57:54 -0300

    📄 shell script modified 🔧 file modified 📄 markdown file modified
    📄 shell script modified
    📄 markdown file modified
    🔧 file modified

---
+++ java/hadoop/start-container.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# create docker network hadoop
+sudo docker network create --driver=bridge hadoop
+
+# the default node number is 3
+N=${1:-3}
+
+# start hadoop master container
+sudo docker rm -f hadoop-master > /dev/null
+echo "start hadoop-master container..."
+sudo docker run -itd \
+           --net=hadoop \
+           -p 9870:9870 \
+           -p 8088:8088 \
+           --name hadoop-master \
+           --hostname hadoop-master \
+           lyingbo/hadoop:3.2.0 > /dev/null
+
+# start hadoop slave container
+i=1
+while [ $i -lt $N ]
+do
+    sudo docker rm -f hadoop-slave$i > /dev/null
+    echo "start hadoop-slave$i container..."
+    sudo docker run -itd \
+               --net=hadoop \
+               --name hadoop-slave$i \
+               --hostname hadoop-slave$i \
+               lyingbo/hadoop:3.2.0 > /dev/null
+    i=$(( $i + 1 ))
+done 
+
+# get into hadoop master container
+sudo docker exec -it hadoop-master bash
+
