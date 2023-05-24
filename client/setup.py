62fca93e60a2f2019db14274f89fe7de0b37f61d
estevam5s <estevamsouzalaureth@gmail.com>
2023-05-24 15:22:34 -0300

    versão melhorada

--- client/setup.py
+++ client/setup.py
@@ -858,24 +858,6 @@ def mainLogin():
           img = get_img_as_base64("client/src/public/tree.png")
           snow_animation = load_lottiefile("client/src/public/lottie-snow.json")
 
-          page_bg_img = f"""
-              <style>
-                  [data-testid="stSidebar"] > div:first-child {{
-                      background-image: url("data:image/png;base64,{img}");
-                  }}
-                  [data-testid="stSidebarNav"] span {{
-                      color:white;
-                  }}
-                  [data-testid="stHeader"] {{
-                      background: rgba(0,0,0,0);
-                  }}
-                  [data-testid="stToolbar"] {{
-                      right: 2rem;
-                  }}
-              </style>
-          """
-          st.markdown(page_bg_img, unsafe_allow_html=True)
-
           st_lottie(snow_animation, height=600, key="initial")
 
         if selecionar == "🗂️ Previsão de Vendas":
